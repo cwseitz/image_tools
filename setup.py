94097d3267fb1c2c5469975854a780de5d75ea55
cwseitz <cwseitz2@gmail.com>
2022-06-30 13:04:56 -0400

    update

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup, Extension
+import numpy
+
+def main():
+    
+    setup(name="image_tools",
+          version="1.0.0",
+          description="Image processing tools for my PhD thesis",
+          author="Clayton Seitz",
+          author_email="cwseitz@iu.edu")
+
+
+
+if __name__ == "__main__":
+    main()
